d49d1eacbc3ebe3b09407a5270f91b72f87c3937
Joan Miquel Torres Rigo <jmtorres@112ib.com>
2025-09-24 17:06:11 +0200

    🪲 Fixed incorrect error event emission.
      👉 Fixes bug #2.

--- node_postgres_ha.js
+++ node_postgres_ha.js
@@ -125,7 +125,7 @@ class Pool extends pg.Pool {
                 try {
                     return await super.query(...args);
                 } catch (err) {
-                    this.emit("error", err);
+                    parentPool.emit("error", err);
                     throw err;
                 };
             };
